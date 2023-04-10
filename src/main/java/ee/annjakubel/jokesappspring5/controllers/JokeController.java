0d4a92be1eb1e416dccce89cfb609cdd8d375dcc
AnnJakubel <annabel.jakubel@gmail.com>
2023-04-10 11:43:20 +0300

    Add JokeController. Closes #3

---
+++ src/main/java/ee/annjakubel/jokesappspring5/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package ee.annjakubel.jokesappspring5.controllers;
+
+import ee.annjakubel.jokesappspring5.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    //This is meant by mapping to root - aka the homepage of the url
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
